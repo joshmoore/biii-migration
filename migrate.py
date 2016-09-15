ce5394c1d2854e74499d4f6998dbf696b1b322da
jmoore <josh@glencoesoftware.com>
2016-09-15 15:14:00 +0200

    fixed: nodes created first

--- migrate.py
+++ migrate.py
@@ -140,8 +140,10 @@ total = len(glob(PATTERN))
 handled = set()
 printed = set()
 
-
+# 1. Load primary data
 nodes = [Node(fname) for fname in glob(PATTERN)]
+
+# 2. Parse the various columns
 for node in nodes:
     for _nid, key, value in node:
         counts[key] += 1
@@ -171,45 +173,19 @@ for k, v in values:
         print "%30s\t%8s\t%30s" % (k, v, ", ".join(T))
 
 
+# 3. Setup db and create nodes
 conn, cur = open_db()
+
 for node in nodes:
     cols = list()
     vals = list()
     for _nid, key, value in node:
-        if not value:
-            continue
         if key in columns:
             cols.append(key)
             vals.append(value)
         elif "#%s" in columns:
             cols.append(key.replace("#", ""))
             vals.append(value)
-        elif "field_language" == key:
-            for tid in value:
-                tid = tid["target_id"]
-                try:
-                    cur.execute(lang_ins, [_nid, tid, _nid, tid])
-                    conn.commit()
-                except psycopg2.IntegrityError, ie:
-                    print fname, _nid, key, value
-                    raise ie
-        elif key in TAG_FIELDS:
-            for tid in value:
-                tid = tid["tid"]
-                cur.execute(tags_ins, [_nid, tid, _nid, tid])
-                conn.commit()
-        elif key in SAFE_TEXT_FIELDS:
-            for tid in value:
-                saf = tid.get("safe_value", None)
-                val = tid["value"]
-                fmt = tid["format"]
-                cur.execute(safe1_ins, [_nid, key, saf, val, fmt, _nid, key])
-                conn.commit()
-        elif key in ("field_data_url",):
-            for tid in value:
-                val = tid["value"]
-                cur.execute(safe2_ins, [_nid, key, None, val, None, _nid, key])
-                conn.commit()
 
     query = (
         "insert into node (%s) select %s where not exists ("
@@ -222,3 +198,32 @@ for node in nodes:
     )
     cur.execute(query, vals)
     conn.commit()
+
+# 4. Add links etc.
+for node in nodes:
+    for _nid, key, value in node:
+        if "field_language" == key:
+            for tid in value:
+                tid = tid["target_id"]
+                try:
+                    cur.execute(lang_ins, [_nid, tid, _nid, tid])
+                except psycopg2.IntegrityError, ie:
+                    print fname, _nid, key, value
+                    raise ie
+        elif key in TAG_FIELDS:
+            for tid in value:
+                tid = tid["tid"]
+                cur.execute(tags_ins, [_nid, tid, _nid, tid])
+        elif key in SAFE_TEXT_FIELDS:
+            for tid in value:
+                saf = tid.get("safe_value", None)
+                val = tid["value"]
+                fmt = tid["format"]
+                cur.execute(safe1_ins, [_nid, key, saf, val, fmt, _nid, key])
+        elif key in ("field_data_url",):
+            for tid in value:
+                val = tid["value"]
+                cur.execute(safe2_ins, [_nid, key, None, val, None, _nid, key])
+    conn.commit()
+
+conn.close()
