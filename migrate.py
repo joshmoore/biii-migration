ef86750d324828bb6955e0037723100ffee07ecd
jmoore <josh@glencoesoftware.com>
2016-09-15 12:30:55 +0200

    wip: flake8 and clean before objectifying

--- migrate.py
+++ migrate.py
@@ -1,38 +1,135 @@
 #!/usr/bin/env python
 
 from glob import glob
-from json import load, dump
+from json import load
 from collections import defaultdict
 import psycopg2
 
-def parse(fname):
-    with open(fname, "r") as f:
-        data = load(f)
-        _nid = data["nid"]
-        for key, value in data.items():
-            if not value: continue
-            if isinstance(value, dict):
-                if "und" in value:
-                    value = value["und"]
-            yield _nid, key, value
+#
+# Constants
+#
 
 PATTERN = "json/*.json"
+
 SAFE_TEXT_FIELDS = (
     "field_url", "field_url_link",
     "field_workflow_author",
     "field_author_s_", "body"
 )
+
 TAG_FIELDS = (
     "field_tags",
     "field_ttest",  # Used minimally in workflows
 )
-
 
 TODO = """
  - body has more fields
  - cleanup 'mhmzmgso'
  - field_task_annotation_descriptio = sample data term or url...
 """
+
+#
+# SQL strings
+#
+
+schema_sql = "select * from information_schema.tables where table_name=%s"
+
+node_sql = "create table node (nid text primary key, %s text)"
+
+term_sql = "create table term (tid text primary key, term text)"
+
+tags_sql = (
+    "create table tags (node text references node(nid), "
+    "term text references term(tid))"
+)
+
+langs_sql = (
+    "create table langs (node text references node(nid), "
+    "lang text references node(nid))"
+)
+
+safe_sql = (
+    "create table safe_text (nid text references node(nid), "
+    "field text, safe_value text, value text, format text)"
+)
+
+node_idx = "create index node_index on node(nid)"
+
+term_idx = "create index term_index on term(tid)"
+
+tags_idx = "create index tag_index on tags(node, term)"
+
+lang_ins = (
+    "insert into langs (node, lang) select %s, %s where not exists ("
+    "  select node, lang from langs where node = %s and lang = %s"
+    ")"
+)
+
+safe1_ins = (
+    "insert into safe_text (nid, field, safe_value, value, format) "
+    "select %s, %s, %s, %s, %s where not exists ("
+    "  select nid, field from safe_text where nid = %s and field = %s"
+    ")"
+)
+
+safe2_ins = (
+    "insert into safe_text (nid, field, safe_value, value, format) "
+    "select %s, %s, %s, %s, %s where not exists ("
+    "  select nid, field from safe_text where nid = %s and field = %s"
+    ")"
+)
+
+tags_ins = (
+    "insert into tags (node, term) select %s, %s where not exists ("
+    "  select node, term from tags where node = %s and term = %s"
+    ")"
+)
+
+#
+# Helpers
+#
+
+
+def parse(fname):
+    """
+    Generation method which provides key/value pairs for each
+    node id in a json file.
+    """
+    with open(fname, "r") as f:
+        data = load(f)
+        _nid = data["nid"]
+        for key, value in data.items():
+            if not value:
+                continue
+            if isinstance(value, dict):
+                if "und" in value:
+                    value = value["und"]
+            yield _nid, key, value
+
+
+def open_db():
+    """
+    Open a cursor to the biii database, creating tables once.
+    """
+
+    conn = psycopg2.connect("dbname='biii'")
+    cur = conn.cursor()
+    cur.execute(schema_sql, ('node',))
+    if not bool(cur.rowcount):
+        cols = " text, ".join([x for x in sorted(columns) if x != "nid"])
+        cur.execute(node_sql % cols)
+        cur.execute(term_sql)
+        cur.execute(tags_sql)
+        cur.execute(langs_sql)
+        cur.execute(safe_sql)
+        with open("terms.tsv", "r") as f:
+            cur.copy_from(f, "term")
+        cur.execute(node_idx)
+        cur.execute(term_idx)
+        cur.execute(tags_idx)
+        conn.commit()
+    return conn, cur
+
 
 columns = set()
 counts = defaultdict(int)
@@ -52,7 +149,7 @@ for fname in glob(PATTERN):
             handled.add(key)
         elif key in SAFE_TEXT_FIELDS \
             or key in TAG_FIELDS \
-            or key == "field_data_url":
+                or key == "field_data_url":
             # These are handled specially below.
             handled.add(key)
         elif type(value) == list:
@@ -71,28 +168,14 @@ for k, v in values:
     if k in printed:
         print "%30s\t%8s\t%30s" % (k, v, ", ".join(T))
 
-conn = psycopg2.connect("dbname='biii'")
-cur = conn.cursor()
-cur.execute("select * from information_schema.tables where table_name=%s", ('node',))
-if not bool(cur.rowcount):
-    cols = " text, ".join([x for x in sorted(columns) if x != "nid"])
-    cur.execute("create table node (nid text primary key, %s text)" % cols)
-    cur.execute("create table term (tid text primary key, term text)")
-    cur.execute("create table tags (node text references node(nid), term text references term(tid))")
-    cur.execute("create table langs (node text references node(nid), lang text references node(nid))")
-    cur.execute("create table safe_text (nid text references node(nid), field text, safe_value text, value text, format text)")
-    with open("terms.tsv", "r") as f:
-        cur.copy_from(f, "term")
-    cur.execute("create index node_index on node(nid)")
-    cur.execute("create index term_index on term(tid)")
-    cur.execute("create index tag_index on tags(node, term)")
-    conn.commit()
 
+conn, cur = open_db()
 for fname in glob(PATTERN):
     cols = list()
     vals = list()
     for _nid, key, value in parse(fname):
-        if not value: continue
+        if not value:
+            continue
         if key in columns:
             cols.append(key)
             vals.append(value)
@@ -102,13 +185,8 @@ for fname in glob(PATTERN):
         elif "field_language" == key:
             for tid in value:
                 tid = tid["target_id"]
-                query = (
-                    "insert into langs (node, lang) select %s, %s where not exists ("
-                    "  select node, lang from langs where node = %s and lang = %s"
-                    ")"
-                )
                 try:
-                    cur.execute(query, [_nid, tid, _nid, tid])
+                    cur.execute(lang_ins, [_nid, tid, _nid, tid])
                     conn.commit()
                 except psycopg2.IntegrityError, ie:
                     print fname, _nid, key, value
@@ -116,39 +194,19 @@ for fname in glob(PATTERN):
         elif key in TAG_FIELDS:
             for tid in value:
                 tid = tid["tid"]
-                query = (
-                    "insert into tags (node, term) select %s, %s where not exists ("
-                    "  select node, term from tags where node = %s and term = %s"
-                    ")"
-                )
-                cur.execute(query, [_nid, tid, _nid, tid])
+                cur.execute(tags_ins, [_nid, tid, _nid, tid])
                 conn.commit()
         elif key in SAFE_TEXT_FIELDS:
-            if key == "field_ttest":
-                import pdb
-                pdb.set_trace()
             for tid in value:
                 saf = tid.get("safe_value", None)
                 val = tid["value"]
                 fmt = tid["format"]
-                query = (
-                    "insert into safe_text (nid, field, safe_value, value, format) "
-                    "select %s, %s, %s, %s, %s where not exists ("
-                    "  select nid, field from safe_text where nid = %s and field = %s"
-                    ")"
-                )
-                cur.execute(query, [_nid, key, saf, val, fmt, _nid, key])
+                cur.execute(safe1_ins, [_nid, key, saf, val, fmt, _nid, key])
                 conn.commit()
         elif key in ("field_data_url",):
             for tid in value:
                 val = tid["value"]
-                query = (
-                    "insert into safe_text (nid, field, safe_value, value, format) "
-                    "select %s, %s, %s, %s, %s where not exists ("
-                    "  select nid, field from safe_text where nid = %s and field = %s"
-                    ")"
-                )
-                cur.execute(query, [_nid, key, None, val, None, _nid, key])
+                cur.execute(safe2_ins, [_nid, key, None, val, None, _nid, key])
                 conn.commit()
 
     query = (
